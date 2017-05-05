df1c121ecd27a81e84244659d7ad131bd05e8c59
SonthayaKhooha <sonthaya.khoo56@sskru.ac.th>
2017-05-06 03:21:23 +0700

    new project

---
+++ app/src/main/java/com/example/sonthaya/queuebank/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.sonthaya.queuebank;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView showvalue;
+    int counter = 0 ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        showvalue = (TextView)findViewById(R.id.queue);
+    }
+
+    public void count (View view){
+        counter++;
+        showvalue.setText(Integer.toString(counter));
+
+//        AlertDialog dialog = new AlertDialog.Builder(this).setMessage("คิวของคุณ คือ  "+counter).show();
+//        TextView textView = (TextView) dialog.findViewById(android.R.id.message);
+//        textView.setTextSize(40);
+
+        AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+        dialog.setCancelable(false);
+        dialog.setTitle("คิวของท่าน คือ   "+counter);
+        dialog.setMessage("โปรดรอจนกว่าจะถึงคิวของท่าน ขอบคุณที่ไว้วางใจธนาคารของเรา");
+        dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                //Action for "ok".
+            }
+        });
+
+        final AlertDialog alert = dialog.create();
+        alert.show();
+    }
+
+}
