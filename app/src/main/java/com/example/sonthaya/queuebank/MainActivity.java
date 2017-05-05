9054faae39dfb9f323a794438612b3d2b3216acb
SonthayaKhooha <sonthaya.khoo56@sskru.ac.th>
2017-05-06 03:49:28 +0700

    add time

--- app/src/main/java/com/example/sonthaya/queuebank/MainActivity.java
+++ app/src/main/java/com/example/sonthaya/queuebank/MainActivity.java
@@ -4,10 +4,14 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,10 +34,26 @@ public class MainActivity extends AppCompatActivity {
 //        TextView textView = (TextView) dialog.findViewById(android.R.id.message);
 //        textView.setTextSize(40);
 
+        Calendar c = Calendar.getInstance();
+        System.out.println("Current time => "+c.getTime());
+
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String formattedDate = df.format(c.getTime());
+        // formattedDate have current date/time
+//        Toast.makeText(this, formattedDate, Toast.LENGTH_SHORT).show();
+
+
+        // Now we display formattedDate value in TextView
+//        TextView txtView = new TextView(this);
+//        txtView.setText("Current Date and Time : "+formattedDate);
+//        txtView.setGravity(Gravity.CENTER);
+//        txtView.setTextSize(20);
+//        setContentView(txtView);
+
         AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
         dialog.setCancelable(false);
         dialog.setTitle("คิวของท่าน คือ   "+counter);
-        dialog.setMessage("โปรดรอจนกว่าจะถึงคิวของท่าน ขอบคุณที่ไว้วางใจธนาคารของเรา");
+        dialog.setMessage("เวลา "+formattedDate+ " โปรดรอจนกว่าจะถึงคิวของท่าน ขอบคุณที่ไว้วางใจธนาคารของเรา ");
         dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int id) {
